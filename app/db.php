764fb657076e1be19f8f376ac67a447ab3aa11da
Andrius0630 <kolendarik123@gmail.com>
2025-11-08 00:47:19 +0200

    Docker compose reverted

--- app/db.php
+++ app/db.php
@@ -1,24 +1,9 @@
 <?php
-// Function to safely read the content of a Docker Secret file
-function get_docker_secret($secret_name)
-{
-    $file_path = "/run/secrets/" . $secret_name;
-    if (file_exists($file_path)) {
-        return trim(file_get_contents($file_path));
-    }
-    return null;
-}
-
 $host = getenv("DB_HOST");
 $port = getenv("DB_PORT");
 $dbname = getenv("DB_NAME");
-
-$user = get_docker_secret("db_user_secret");
-$password = get_docker_secret("db_password_secret");
-
-if (!$user || !$password) {
-    die("❌ Database secrets (user/password) not found in /run/secrets.");
-}
+$user = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
 
 try {
     $pdo = new PDO(
