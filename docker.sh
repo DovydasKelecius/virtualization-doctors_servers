bb55090320acc06962e644eed127b21c8e22b15f
juliuzzak <juliuskasak@gmail.com>
2025-11-27 22:01:29 +0200

    Remove duplicate docker stop and rm commands

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
  docker stop hospital_php &&  docker stop hospital_nginx &&  docker rm hospital_php && sudo docker rm hospital_nginx
- docker stop hospital_php &&  docker stop hospital_nginx &&  docker rm hospital_php && sudo docker rm hospital_nginx
+
